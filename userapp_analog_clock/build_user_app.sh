6d1bb82f3918dd6b8166537dd027788af530cdb6
andrii.deviatov <andrii.deviatov@globallogic.com>
2017-11-21 14:04:04 +0200

    ssd1306: fixed build scripts and Makefiles

--- userapp_analog_clock/build_user_app.sh
+++ userapp_analog_clock/build_user_app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-export CROSS_COMPILE=${HOME}/BBB/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux/bin/arm-linux-gnueabihf-
+#export CROSS_COMPILE=${HOME}/BBB/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux/bin/arm-linux-gnueabihf-
 
-echo ${CROSS_COMPILE}
+echo "CROSS_COMPILE = ${CROSS_COMPILE}"
 
 
 ${CROSS_COMPILE}gcc analog_clock.c -o analog_clock -lm
